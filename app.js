3006c90513490e0e5e76e0e381c22ba0385449ad
MithrasdeLuca-dev <mithrasdeluca.dev@gmail.com>
2023-07-09 13:53:02 -0400

    Implementando váriavel de ambiente

--- app.js
+++ app.js
@@ -1,10 +1,11 @@
-
+const mongoose = require('mongoose');
 const express = require('express');
 const app = express();
+require('./database/config')
 
-app.get('/', (req, res) => res.send('Hello World!'));
+app.get('/', (req, res) => res.send('Hello World!'))
 
-
-const LocalHttp = 'http://localhost:3010' 
+const LocalHttp = 'http://localhost:3010'
 
 app.listen(3010, () => console.log('Example app listening on port 3010! ' + LocalHttp));
+
